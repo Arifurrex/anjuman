220429455b133d65b14a4bf20e650987ae3f209f
Arifur Rahman <arifurrex@gmail.com>
2021-12-27 10:17:10 +0600

    history read more and read less js file add done

--- js/history.js
+++ js/history.js
@@ -1,4 +1,20 @@
-const historyButton = document.getElementById("historyButton");
+const historyBtn = document.getElementById("historyButton");
+const dot = document.getElementById("dot");
+const moreText = document.getElementById("more");
+
 historyButton.addEventListener('click', function () {
-    
-})
+
+    if (dot.style.display === "none") {
+        historyBtn.innerHTML = "বিস্তারিত জানতে";
+        moreText.style.display = "none";
+        dot.style.display = "inline"; 
+    } else {
+         dot.style.display = "none"; 
+         historyBtn.innerHTML = "read less";
+        moreText.style.display = "inline"; 
+      }
+
+});
+
+
+

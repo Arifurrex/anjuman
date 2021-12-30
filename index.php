d1032e8e8768dc656c2bb319255ba05598795dce
Arifur Rahman <arifurrex@gmail.com>
2021-12-31 02:36:40 +0600

    comette done

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ include 'pillar.php';
 include 'event.php';
 include 'includes/dbh.inc.php';
 include 'news.php';
+include 'comitte.php';
 // include 'testomonial.php';
 // include 'CustomerUI.php';
 include 'footer.php'; 

e19ef35146853bbcc8655dd5547a08805d7089be
Arifur Rahman <arifurrex@gmail.com>
2021-12-31 20:45:30 +0600

    email and contact done

--- email&contact.php
+++ email&contact.php
@@ -1,6 +1,6 @@
 <section class="contactEmail container">
     <div class="flex-wrap">
-        <div class="contact-top">01753196609</div>
-        <div class="email">anjumanehefajoth@gmail.com</div>
+        <div class="contact-top"><span><i class="fas fa-phone-square"></i></span> ০১৭৫২০৯১১৪২</div>
+        <div class="email-top"><span><i class="fas fa-envelope"></i></span> anjumanehefajoth@gmail.com</div>
     </div>
 </section>

adea521eb4c65e607a0ad22ed773ae31a5068590
Arifur Rahman <arifurrex@gmail.com>
2022-01-17 14:21:59 +0600

    comment oput dbh.inc.php file from index.html

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include 'frontend/email&contact.php';
 include 'frontend/history.php';
 include 'frontend/pillar.php';
 include 'frontend/event.php';
-include 'includes/dbh.inc.php';
+// include 'includes/dbh.inc.php';
 // include 'frontend/news.php';
 include 'frontend/comitte.php';
 include 'frontend/footer.php'; 

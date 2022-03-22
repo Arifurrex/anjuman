07d855d297c89adcc3973ae2b42fff3e6c4de207
arifurrex <arifurrex@gmail.com>
2022-03-22 16:23:25 +0600

    upojella done

--- includes/updateupojella.inc.php
+++ includes/updateupojella.inc.php
@@ -6,6 +6,6 @@ $district_id=$_POST['district_id'];
 
 $sqli="UPDATE `upojella` SET `upojella_name`= '$ca',district_id=$district_id WHERE upojella_id=$id";
 $result=mysqli_query($conn,$sqli) or die('query failed');
-header('location:../admin/upojella.php?msg=updasuccess!!');
+header('location:../admin/upojella/upojella.php?msg=updasuccess!!');
 exit();
  
